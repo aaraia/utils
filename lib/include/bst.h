a634562a049ddd9a19acd669f71a9b405e907db0
aaraia <aaraia@gmail.com>
2018-04-13 18:20:51 +0100

    removed earlier commets

--- lib/include/bst.h
+++ lib/include/bst.h
@@ -104,86 +104,48 @@ namespace utils {
             return false;
         }
 
-        // This is how I would write it.
-        // Slightly different as I would use pointers not smart pointers
-        // But I am sure you can modify to copensate.
+       
         template<typename Key, typename Data>
         typename BST<Key, Data>::Node* BST<Key, Data>::removeHelper(typename BST<Key, Data>::Node* node, const Key& k)
         {
-            // When you fall off the end
-            // the key was not found. So just return null.
             if (node == nullptr) {
                 return nullptr;
             }
 
-
-            // In most cases we return the current node.
-            // So lets keep track of that separately. If this
-            // is the node we are going to delete then we update
-            // this with the node we are going to replace it with.
             Node* result = node;
 
-
             if (k < node->mKey) {
-                // Note if we don't remove node-left then it will
-                // return back the same value and nothing happens.
-                // If we do delete node->left we will return the replacement value.
-                // Note: In this case the node is not deleted so result is not changed.
                 node->mLeft = removeHelper(node->mLeft, k);
             }
             else if (k > node->mKey) {
-                // See comment above about left.
                 node->mRight = removeHelper(node->mRight, k);
             }
             else {
-
-                // We found the node we delete.
-                // In here we must change `result` to reflect the new node.
-
-                // If either of the sub-trees is empty this becomes easy as the other
-                // sub-tree becomes the new current node.
+               
                 if (node->mLeft == nullptr || node->mRight == nullptr) {
                     result = node->mLeft == nullptr ? node->mRight : node->mLeft;
                     delete node;
                 }
                 else {
-                    // OK both sub trees are not null.
-                    // We must find a node to be used here.
-                    // A node where all the nodes to the right are larger
-                    // and all the nodes to the left are smaller.
-                    //
-                    // If we look in the right sub-tree. Then the left most
-                    // node in this sub tree fulfills these requirements.
-                    // We can also easily remove it as it will not have a
-                    // left-subtree. Once we have it assign the
-                    // left and right sub-trees of the current node to it
-                    // It will be inserted into the tree with the return
-                    // below.
+                  
                     node->mRight = findLeftMostNode(node->mRight, result);
                     result->mLeft = node->mLeft;
                     result->mRight = node->mRight;
                     delete node;
                 }
-
             }
-            // If you did not fall of the end.
-            // Then return the result
+           
             return result;
         }
 
         template<typename Key, typename Data>
         typename BST<Key, Data>::Node* BST<Key, Data>::findLeftMostNode(typename BST<Key, Data>::Node* node, typename BST<Key, Data>::Node*& result)
         {
-            if (node->mLeft == nullptr) {
-                // We found the left most node. So this is the result.
-                result = node;
-                // We return its right-subtree so that it can be glued
-                // back into the BST.
+            if (node->mLeft == nullptr) {                
+                result = node;                
                 return node->mRight;
             }
-
-            // We have not found the left most node.
-            // So keep looking.
+          
             node->mLeft = findLeftMostNode(node->mLeft, result);
             return node;
         }
